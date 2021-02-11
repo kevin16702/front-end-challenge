07a105de4af84b50bfc14ea71c794b155c43d956
kevin16702 <40112089+kevin16702@users.noreply.github.com>
2021-02-11 08:52:48 +0100

    initial commit

---
+++ includes/script.js
@@ -0,0 +1,24 @@
+let container = document.getElementByID('container');
+let title = document.getElementByID('question');
+let buttoncontainer = document.getElementByID('buttoncontainer');
+
+let questions = [
+    ["homo's moeten geaccepteerd worden in de maatschappij", 'prog-cons'],
+    ['des te vrijer de economie des te vrijer de mens', 'lib-auth'],
+    ['de overheid moet zorgen voor hun burgers', 'left-right'],
+    ['onderwijs moet gratis', 'left-right'],
+    ['politie moet harder optreden', 'lib-auth'],
+];
+
+let buttons = [
+    'helemaal mee eens',
+    'mee eens',
+    'neutraal / niet zeker',
+    'mee oneens',
+    'helemaal mee oneens'
+]
+
+for(let i = 0; i < buttons.length; i++)
+{
+
+}
