60069229efab9ad3da2e219176940fa659857792
kevin16702 <40112089+kevin16702@users.noreply.github.com>
2021-02-12 09:45:51 +0100

    Added backbutton, changed functions,

--- includes/script.js
+++ includes/script.js
@@ -1,38 +1,124 @@
+// Creating variables
+
 let count = -1;
 
 const title = document.getElementById('question');
 const buttonContainer = document.getElementById('button-container');
 const statementParagraph = document.getElementById('statement');
+const backButton = document.getElementById('back-arrow');
+const homeScreenButton = document.getElementById('homescreen-button')
 
 let buttons = [
     'eens',
     'neutraal / niet zeker',
     'oneens',
+    'skip',
 ]
-if(count = -1){
-    var startBtn = document.createElement('button');
+
+// create startButton
+
+function createStartButton(){
+    let startBtn = document.createElement('button');
     startBtn.innerHTML = 'start';
     startBtn.classList.add('button');
     buttonContainer.appendChild(startBtn);
+    return startBtn;
+}
+// Laying out the start screen
+
+if(count = -1){
+    startBtn = createStartButton();
     startBtn.onclick = function() {
-        count++;
-        clickEvent();
-        startGame();
+        homeScreenButton.classList.remove('hidden');
+        let placeholder = 3;
+        subStep(placeholder, count);
+        Quiz();
     }
 }
-function startGame(){
+
+
+// Starts the quiz and appends the buttons to the container
+
+function Quiz(){
     buttonContainer.removeChild(startBtn);
-    for(let i = 0; i < buttons.length; i++)
-    {
-        let btn = document.createElement('button');
-        btn.innerHTML = buttons[i];
-        btn.classList.add('button');
-        buttonContainer.appendChild(btn);
-        btn.onclick = clickEvent;
+        for(let i = 0; i < buttons.length; i++)
+        {
+            let btn = document.createElement('button');
+            btn.innerHTML = buttons[i];
+            btn.classList.add('button');
+            buttonContainer.appendChild(btn);
+            btn.onclick = function () {
+                subStep(i); 
+        }
     }
 }
-function clickEvent(){
+
+
+// Adds a function to perform code between steps
+
+function subStep(i){
+    count++
+    console.log(count)
+
+    if(count == 1){
+        backButton.classList.remove('hidden');
+    }
+    clickEvent(i);
+}
+
+
+// Performs the click event
+
+function clickEvent(i){
+    switch(i){
+        case 0:
+        
+        break;
+        case 1:
+            
+        break;
+        case 2:
+
+        break;
+        case 3:
+        break;
+    }
+    if(count == subjects.length){
+        getResults();
+        return;
+    };
+    title.innerHTML = subjects[count].title;
+    statement.innerHTML = subjects[count].statement;    
+}
+
+// This function performs the go back event
+
+backButton.onclick = function(){
+    count--;
+    if(count < 1){
+        backButton.classList.add('hidden');
+    }
     title.innerHTML = subjects[count].title;
     statement.innerHTML = subjects[count].statement;
-    count++
+}
+
+// Gets and displays the results
+
+function getResults(){
+    for(i = 0; i < buttons.length; i++){
+    buttonContainer.removeChild(buttonContainer.childNodes[0]);
+    }
+}
+
+// Goes back to homescreen
+
+homeScreenButton.onclick = function(){
+    homeScreenButton.classList.add('hidden');
+    count = 0;  
+    title.innerHTML = 'Stemwijzer';
+    statementParagraph.innerHTML = 'Welkom bij de stemwijzer';
+    for(i = 0; i < buttons.length; i++){
+        console.log(i);
+        buttonContainer.removeChild(buttonContainer.childNodes[0]);
+    }
 }
